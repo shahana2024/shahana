786dbe29101f9431c06bbc9edab806b13ea472ca
shahana2024 <shahanamol1920@gmail.com>
2024-10-15 23:01:39 -0700

    Add files via upload

---
+++ arithmetic operation.py
@@ -0,0 +1,8 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number"))
+sum=a+b
+sub=a-b
+multy=a*b
+div=a/b
+mod=a%b
+print("sum=",sum,"\nsubtraction=",sub,"\nmultiplication=",multy,"\ndivision=",div,"\nmodless=",mod)
